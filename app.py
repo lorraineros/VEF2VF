71e79051a2dcc47b976541d6e28df267811b44b3
Lorraine Rós <lorrainert2002@gmail.com>
2019-09-04 15:58:40 +0000

    ok

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ def home():
     return """
     <h1>Hello user</h1>
     <img src="https://image.flaticon.com/icons/svg/817/817397.svg" height="250" width="250">
-    <h1> Hi my name is Lorraine, click the tabs to know what I like and for logos :) enjoy.
+    <h1> Hi my name is Lorraine, click the tabs to know what I like and for more logos :) enjoy.
     <p><a href="/sida2"title ="Síða 2">Síða 2</a> | <a href="/sida3"title ="Síða 3">Síða 3</a></p>
     """
 
@@ -14,7 +14,7 @@ def sida2():
     return"""
     <h1>Hello user</h1>
     <img src="https://image.flaticon.com/icons/svg/2033/2033324.svg" height="250" width="250">
-    <h1> I really love ice cream. Click the tabs to see more what I like :)
+    <h1> I really love ice cream. Click the tabs to see more logo :)
     <p><a href="/"title ="Síða 1">Síða 1</a> | <a href="/sida3"title ="Síða 3">Síða 3</a></p>
     """
 @app.route("/sida3")
